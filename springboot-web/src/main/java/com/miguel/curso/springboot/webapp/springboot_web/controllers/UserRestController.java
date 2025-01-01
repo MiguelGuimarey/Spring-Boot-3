674eda501f467d03848322053db8debc189d8477
Miguel Guimarey Lesmes <MiguelGuimareyLesmes96@outlook.es>
2025-01-01 17:21:20 +0100

    Added DTO User

--- springboot-web/src/main/java/com/miguel/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ springboot-web/src/main/java/com/miguel/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -1,27 +1,25 @@
 package com.miguel.curso.springboot.webapp.springboot_web.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.miguel.curso.springboot.webapp.springboot_web.models.User;
+import com.miguel.curso.springboot.webapp.springboot_web.models.dto.UserDto;
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
     @RequestMapping(path = "/details", method = RequestMethod.GET)
-    public Map<String, Object> details() {
-
+    public UserDto details() {
+        UserDto userDto = new UserDto();
         User user = new User("Miguel", "Guimarey");
-        Map<String, Object> body = new HashMap<>();
-        body.put("title", "Hola Mundo Spring Boot");
-        body.put("user", user);
 
-        return body;
+        userDto.setUser(user);
+        userDto.setTitle("Hola Mundo Spring Boot");
+
+        return userDto;
     }
 
 }

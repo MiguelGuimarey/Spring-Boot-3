91c0efe595957e9fa39b41ffee44b54b7847e909
Miguel Guimarey Lesmes <MiguelGuimareyLesmes96@outlook.es>
2025-01-06 20:30:44 +0100

    Añadido Demo Archivos properties personalizados

---
+++ springboot-web/src/main/java/com/miguel/curso/springboot/webapp/springboot_web/ValuesConfig.java
@@ -0,0 +1,12 @@
+package com.miguel.curso.springboot.webapp.springboot_web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+    @PropertySource("classpath:values.properties")})
+public class ValuesConfig {
+
+}

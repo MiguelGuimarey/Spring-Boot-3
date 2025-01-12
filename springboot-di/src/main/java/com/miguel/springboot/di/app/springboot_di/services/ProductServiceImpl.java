c88bea5fbef42a80122ce25fe79f12e262d5f7c1
Miguel Guimarey Lesmes <MiguelGuimareyLesmes96@outlook.es>
2025-01-12 14:08:28 +0100

    Añadido Demo leer desde un archivo configuración

--- springboot-di/src/main/java/com/miguel/springboot/di/app/springboot_di/services/ProductServiceImpl.java
+++ springboot-di/src/main/java/com/miguel/springboot/di/app/springboot_di/services/ProductServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import com.miguel.springboot.di.app.springboot_di.models.Product;
@@ -12,6 +14,10 @@ import com.miguel.springboot.di.app.springboot_di.repositories.ProductRepository
 @Service
 public class ProductServiceImpl implements ProductService {
 
+    //@Autowired
+    private Environment environment;
+    @Value("${config.price.tax}")
+    private Double tax;
     private ProductRepository repository;
 
     public ProductServiceImpl(@Qualifier("productList") ProductRepository repository) {
@@ -21,7 +27,8 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public List<Product> findAll() {
         return repository.findAll().stream().map(p -> {
-            Double priceTax = p.getPrice() * 1.25d;
+            //Double priceTax = p.getPrice() * environment.getProperty("config.price.tax", Double.class);
+            Double priceTax = p.getPrice() * tax;
             Product newProduct = (Product) p.clone();
             newProduct.setPrice(priceTax.longValue());
             return newProduct;
